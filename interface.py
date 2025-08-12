4f1522cdaee0b29b13032aca22ad07e17f09e074
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-12 20:56:53 +0530

    Images Input Written

--- interface.py
+++ interface.py
@@ -1,1 +1,4 @@
 import streamlit as st
+st.title("Signature Verification")
+upload_file_0 = st.file_uploader("Upload Original Signature",type=["jpg","jpeg","png"])
+upload_file_1 = st.file_uploader("Upload Signature To Be Verified",type=["jpg","jpeg","png"])
